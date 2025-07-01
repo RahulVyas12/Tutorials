56cbe57d135ae74166867e04ab2a9c6eba37e5bf
lenovo <lenovo@DESKTOP-9QAGOMJ>
2025-07-01 20:57:54 +0530

    Add project files.

---
+++ Q2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Tutorial
+{
+    internal class Q2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name: Rahul Vyas");
+            Console.WriteLine("DOB: 08/06/2006");
+            Console.WriteLine("Address: 4, xyx society ");
+            Console.WriteLine("        Kalawad Road");
+            Console.WriteLine("City: Rajkot");
+
+            Console.WriteLine("Pincode: 360 001");
+            Console.WriteLine("State: Gujarat");
+            Console.WriteLine("Country: India");
+            Console.WriteLine("Email: abc@ymail.com");
+        }
+    }
+}
